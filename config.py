466b8bcf08f1c0d38dedd06c29012b4d9c735ef9
jzystar <jiangzyang@gmail.com>
2025-04-10 00:36:25 +0800

    update config

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 zsxq_access_token='79E99EC4-BFDF-4241-BE17-26A0B1A92A67_BC485B23CA208E72'
-group_id='88888558554112'
+group_id='111111111'
 user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36'
